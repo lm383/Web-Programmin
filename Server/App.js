ed4d68c87f06d40583994dc4a2aeb59fc4833700
lm383 <71259991+lm383@users.noreply.github.com>
2020-10-07 16:28:27 +0100

    added some comments

--- Server/App.js
+++ Server/App.js
@@ -1,10 +1,12 @@
+// imported the http library and set up the port for the server
 const Http = require('http')
 const Port = 3000
-
+// the server all functionallity goes here
 const Server = Http.createServer(function(req,res){
 
 })
-
+// this is checking that the server is up and listening if is it will output
+// everything is working if not will output an error has happened
 Server.listen(Port, function(Error){
   if (Error){
     console.log('something Went wrong :(', Error);
