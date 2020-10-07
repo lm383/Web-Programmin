1cc8848d62e4fd60e40b023440d0ecdcae18591c
lm383 <71259991+lm383@users.noreply.github.com>
2020-10-07 21:17:50 +0100

    Update App.js

--- Server/App.js
+++ Server/App.js
@@ -1,10 +1,20 @@
 // imported the http library and set up the port for the server
-const Http = require('http')
-const Port = 3000
+const Http = require('http');
+const Port = 3000;
 // the server all functionallity goes here
 const Server = Http.createServer(function(req,res){
+  res.writeHead(200, {'Content-Type':'text/html'}); //status code
+  fs.readFile('index.html', function(Error, data){
+    if(Error){
+      res.writeHead(404);
+      res.write('Error: File Not Found');
+      }
+    res.end(); // end command
+    }
+    );
 
-})
+  }
+);
 // this is checking that the server is up and listening if is it will output
 // everything is working if not will output an error has happened
 Server.listen(Port, function(Error){
@@ -12,5 +22,6 @@ Server.listen(Port, function(Error){
     console.log('something Went wrong :(', Error);
   }else{
     console.log('Server is listening on port '+Port);
+    };
   }
-})
+);
