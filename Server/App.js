f534562442da3ab698412dcf649c8f4583d1eb54
lm383 <71259991+lm383@users.noreply.github.com>
2020-10-13 22:01:45 +0100

    added some database info in comments

--- Server/App.js
+++ Server/App.js
@@ -25,3 +25,12 @@ Server.listen(Port, function(Error){
     };
   }
 );
+/*
+Database info for future:
+Server: sql2.freemysqlhosting.net
+Database/username: sql2369364
+password:bX9!iX9%
+portNm: 3306
+table name: Logins
+
+*/
