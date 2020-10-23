453c2d86f82d79e514b2fabcb5900de1a7e25ee8
lm383 <71259991+lm383@users.noreply.github.com>
2020-10-23 23:22:53 +0100

    attempt to fix signup form hide/show 5

--- Public/ClientSide.js
+++ Public/ClientSide.js
@@ -1,9 +1,10 @@
 function HideShowSign() {
   // here we will hide the SignUp form
   var x = document.getElementById("SignUp");
-  // here we loop through the divs and hide each one we
+  // here we hide the div if it is showing and visversa
   if (x.style.display === "none") {
     x.style.display = "block";
+    x.style.visibility = "visible";
     } else {
     x.style.display = "none";
   }
