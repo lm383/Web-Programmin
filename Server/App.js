ce23923fc6ee42b21e0d13a9ccee38ebfabaa7e0
lm383 <71259991+lm383@users.noreply.github.com>
2020-10-29 20:12:07 +0000

    when run App.js in command line shows index

--- Server/App.js
+++ Server/App.js
@@ -1,5 +1,6 @@
 // imported the http library and set up the port for the server
 const Http = require("http");
+const Fs = require("fs");
 var express = require('express');
 var app = express();
 //var MySQL = require("mysql");
@@ -7,10 +8,11 @@ const Port = 3000;
 
 
 
-
+/*
 app.get('/', function (req, res) {
-    res.sendFile('/../index.html');
+    res.sendFile('../index.html');
 });
+
 
 app.post('/submit-SignUp', function (req, res) {
     var name = req.body.Username + ' ' + req.body.Password;
@@ -21,18 +23,20 @@ app.post('/submit-SignUp', function (req, res) {
 var server = app.listen(5000, function () {
     console.log('Node server is running..');
 });
+*/
 
 // the server all functionallity goes here
-/*
+
 const Server = Http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/html" }); //status code
-  res.readFile("index.html", function (Error, data) {
+  Fs.readFile("../index.html", function (Error, data) {
     if (Error) {
       res.writeHead(404);
       res.write("Error: File Not Found " + data);
+    }else{
+      res.write(data);
     }
-    if (req) {
-    }
+
     res.end(); // end command
   });
 });
@@ -45,4 +49,3 @@ Server.listen(Port, function (Error) {
     console.log("Server is listening on port " + Port);
   }
 });
-*/
