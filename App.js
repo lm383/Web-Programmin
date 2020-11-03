1031aa0d1f3e638543bc6d0d0c29b0eb7f6ad202
lm383 <71259991+lm383@users.noreply.github.com>
2020-11-03 15:08:40 +0000

    server working correctly, database signup val and login still need to do

--- App.js
+++ App.js
@@ -3,13 +3,12 @@ const Http = require("http");
 const Fs = require("fs");
 const Path = require('path');
 const BodyParser = require('body-parser')
-
-var Express = require('express');
-var App = Express();
+const Express = require('express');
+const App = Express();
 //var MySQL = require("mysql");
 const Port = 3000;
 App.use(BodyParser.json());
-App.use(Express.static("Public"));
+App.use(Express.static(Path.join(__dirname, '/Public/')));
 App.use(BodyParser.urlencoded({
     extended: true
 }));
@@ -28,11 +27,14 @@ const Server = App.listen(Port, function (Error) {
 
 // this is the home page
 App.get("/", function(req,res){
-  res.sendFile("/index.html");
+  res.sendFile("index.html");
+});
+App.get("/index.html", function(req,res){
+  res.sendFile("index.html");
 });
 // for when user trys to click the game page link this will allow them to actually go there
-App.get("Game/GamePage", function(req,res){
-  res.sendFile("/Game/GamePage.html");
+App.get("/Game/GamePage", function(req,res){
+  res.sendFile("Game/GamePage.html",{ root : __dirname+"/Public"});
 });
 
 // this if for when the signup form is submitted
@@ -42,14 +44,19 @@ App.post('/SignUpSubmit', function (req, res) {
     var Password = req.body.Password;
     var RePassword = req.body.RePassword;
     // here we save the database file as a variable to make it easier to use
-    const Database = require("./Server/Database.js")
+    //const Database = require("./Server/Database.js")
     // here we call the SignUpTo function so it can add the data to the database
-    var Result = Database.SignUpTo(Username, Password);
-    if (Result){
-      alert("That's you signed up!");
-      res.sendFile("/Game/GamePage.html");
+    var Result = Database.Check(Username, Password);
+    if (Result===null){
+      Database.SignUpTo(Username, Password);
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("welcome "+ Username);
+      res.end('<br><a href="/Game/GamePage.html" rel="nofollow">START</a>');
     }else{
-      res.send("Uh oh! something went wrong :(");
+      // takes you to page where says error happened and gives option to go back to start
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write("Username taken try another.");
+      res.end('<br><a href="index.html" rel="nofollow">Back</a>');
     }
 
 });
--- Server/Database.js
+++ Server/Database.js
@@ -1,7 +1,7 @@
 var MySQL = require("mysql");
 
 /*
-Database info for future:
+Database info:
 Server: sql2.freemysqlhosting.net
 Database/username: sql2369364
 password:bX9!iX9%
@@ -9,56 +9,56 @@ portNm: 3306
 table name: Logins
 
 */
-
+// this is my database connection so I can access it
 var Con = MySQL.createConnection({
   host: "sql2.freemysqlhosting.net",
   user: "sql2369364",
   password: "bX9!iX9%",
   database:"sql2369364"
 });
-/*
-Con.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-*/
+
+// this is where all actions that need done to the database e.g new player signup / login/ highscore update
 module.exports = {
   // here we will check input the signup form into our database
   // we need see if username is already taken if it has send back error
   // if not enter the username and password into the database
   SignUpTo: function SignUpTo(Username, Password) {
-    /*validation
-    var QuerySearch = `SELECT * FROM Logins WHERE Username= ${MySQL.escape(
-      Username
-    )}`;
-    */
     var QueryInsert = `INSERT INTO Logins(Username, Password, HighScore) VALUES ('`+
       Username+`','`+
       Password+`','0')`;
-    //(${MySQL.escape(Username)},${MySQL.escape(Password)},0)
 
-    Con.connect(function (err) {
+    // if the username does not exist we can succeffully address
+
+    Con.query(QueryInsert, function (err, result) {
       if (err) throw err;
-      console.log("Connected!");
+      console.log(result);
+      return true;
+    });
 
-      Con.query(QueryInsert, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        return true;
+    }
+
+  ,
+  Check: function Check(Username, Password){
+    //validation
+    var QuerySearch = `SELECT * FROM Logins WHERE Username= '`+Username+`'`;
+    Con.query(QuerySearch, function (err, result, fields) {
+      // searches database for username if found return result false to say no go
+      if (err) throw err;
+      var rows;
+      Object.keys(result).forEach(function(key) {
+      var rows = 1;
+      console.log(rows);
       });
-      /* validation
-      Con.query(QuerySearch, function (err, result) {
-        if (err) throw err;
-        if (result != null) {
-          console.log("username taken");
-          return "Error: " + Username + " is already Taken";
-        } else {
-
-          // here if there was no results for that Username
-
-        }
-      });
-      */
+      console.log(rows);
+      if (rows===1){
+        return result;
+      }else if (rows === undefined){
+        // if no results found then go ahead
+        return null;
+      };
     });
   }
+
+
+
 };
