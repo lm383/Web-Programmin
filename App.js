f4cb3494c0b0cf711af6b41d050fb0c8ef5dc8df
lm383 <lm383@hw.ac.uk>
2020-11-25 18:25:39 +0000

    log in fully functional

--- App.js
+++ App.js
@@ -2,11 +2,11 @@
 const Http = require("http");
 const Fs = require("fs");
 const Path = require('path');
-const BodyParser = require('body-parser')
+const BodyParser = require('body-parser');
 const Express = require('express');
 const App = Express();
 
-//var MySQL = require("mysql");
+
 const Port = 3000;
 App.use(BodyParser.json());
 App.use(Express.static(Path.join(__dirname, '/Public/')));
@@ -52,13 +52,14 @@ App.post('/SignUpSubmit', function (req, res) {
     var Password = req.body.Password;
     var RePassword = req.body.RePassword;
     // here we save the database file as a variable to make it easier to use
-    //const Database = require("./Server/Database.js")
+    const Database = require("./Server/Database.js")
     // here we call the SignUpTo function so it can add the data to the database
     var Result = Database.Check(Username, Password);
-    if (Result===null){
+    // if no response e.g not true
+    if (!Result){
       Database.SignUpTo(Username, Password);
       res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write("welcome "+ Username);
+      res.write("succeffully signed Up. welcome "+ Username);
       res.end('<br><a href="/Game/GamePage.html" rel="nofollow">START</a>');
     }else{
       // takes you to page where says error happened and gives option to go back to start
@@ -66,6 +67,31 @@ App.post('/SignUpSubmit', function (req, res) {
       res.write("Username taken try another.");
       res.end('<br><a href="index.html" rel="nofollow">Back</a>');
     }
+
+});
+
+// this if for when the login form is submitted
+App.post('/LogInSubmit', function (req, res) {
+  // this is where the variables get saved so we can use them
+    var Username = req.body.UsernameL;
+    var Password = req.body.PasswordL;
+    // here we save the database file as a variable to make it easier to use
+    const Database = require("./Server/Database.js")
+    // here we call the SignUpTo function so it can add the data to the database
+    Database.GetLog(Username, Password, function(err, result){
+      console.log(result);
+      if (result){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("succeffully logged in. Welcome "+ Username);
+        res.end('<br><a href="/Game/GamePage.html" rel="nofollow">START</a>');
+      }else{
+        // takes you to page where says error happened and gives option to go back to start
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write("Sorry either Username/password incorrect");
+        res.end('<br><a href="index.html" rel="nofollow">Back</a>');
+      }
+    }); // the function(err, result) forces the server to wait for result before moving on
+
 
 });
 
