e7325488c430c328375c4de29bbb441685eb147a
JustineLile <71636219+JustineLile@users.noreply.github.com>
2020-11-26 00:13:23 +0000

    Added a flag and score, score is not yet displayed

--- Public/Game/JavaScript/PlayerMovement.js
+++ Public/Game/JavaScript/PlayerMovement.js
@@ -6,40 +6,46 @@ var isCollidingRight = false; //insitialises the booleans
 var isCollidingLeft = false;
 var isCollidingDown = false;
 var isCollidingUp = false;
+var redScore = 0;
 
 function move(e){
 	
 //should move the square down, by adding to top
- if(e.keyCode == 40 && isCollidingDown == false){ //if this button is pressed and player is not colliding
-	 playerTop += 4;
-	 player.style.top = playerTop + "px";
-	 collision(); //calls the collision method in the if statements to update the booleans for the next button press
- }
+ 	if(e.keyCode == 40 && isCollidingDown == false){ //if this button is pressed and player is not colliding 
+		 playerTop += 4;
+		 player.style.top = playerTop + "px";
+		 borderCollision(); //calls the collision method in the if statements to update the booleans for the next button press
+		 flagCollision();
+	}
 
 //moves the square right
-if(e.keyCode == 39 && isCollidingRight == false){
-	left += 2;
-	player.style.left = (parseInt(left) + left) + "px";
-	collision();
-}
+	if(e.keyCode == 39 && isCollidingRight == false){
+		left += 2;
+		player.style.left = (parseInt(left) + left) + "px";
+		borderCollision();
+		flagCollision();
+	}
 
 //moves the square left
-if(e.keyCode == 37 && isCollidingLeft == false){
-	left -= 2;
-	player.style.left = (parseInt(left) + left) + "px";
-	collision();
-}
+	if(e.keyCode == 37 && isCollidingLeft == false){
+		left -= 2;
+		player.style.left = (parseInt(left) + left) + "px";
+		borderCollision();
+		flagCollision();
+	}
 
 //should move the square up, by taking away from top
-if(e.keyCode == 38 && isCollidingUp == false){
-	playerTop -= 4;
-	player.style.top = playerTop + "px";
-	collision();
-}
+	if(e.keyCode == 38 && isCollidingUp == false){
+		playerTop -= 4;
+		player.style.top = playerTop + "px";
+		borderCollision();
+		flagCollision();
+	}
+	
 }
 
 //currently detects whether the player is colliding with the edge of the window
-function collision(){
+function borderCollision(){
 	//if it is at that location then it sets the boolean to true so that it cannot move in that direction
 	if (player.offsetLeft - 4 > window.innerWidth - 8){ //will be modified later for walls
 		isCollidingRight = true; 
@@ -59,12 +65,24 @@ function collision(){
 	else{
 		isCollidingDown = false;
 	}
-	if(player.offsetTop + 4< 8){
+	if(player.offsetTop + 4 < 8){
 		isCollidingUp = true;
 	}
 	else{
 		isCollidingUp = false;
 	}
+}
+//function checks if the player is touching the flag
+function flagCollision(){
+	var playerPos = player.getBoundingClientRect(); //creates a local variable with the position information of the player
+	var flagPos = flag.getBoundingClientRect();
+	if(!(playerPos.right < flagPos.left || playerPos.left > flagPos.right || //checks if the player is intersecting with the flag
+		playerPos.bottom < flagPos.top || playerPos.top > flagPos.bottom)){ 
+			redScore++; //increments the score
+			player.style.left = 0 + "px"; //resets the players position
+			player.style.top = 15 + "px";
+	}
+
 }
 document.onkeydown = move;
 
