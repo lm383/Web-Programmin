ce8eda40d9633052418fafff982fbb9aabc48195
lm383 <lm383@hw.ac.uk>
2020-11-26 04:22:13 +0000

    added update highscore code

--- Server/Database.js
+++ Server/Database.js
@@ -67,9 +67,25 @@ module.exports = {
   },
   //LogInTo: ,
 
-  UpdateHighScore: function UpdateHighScore(Username, HighScore){
+  UpdateHighScore: function GetHighScore(Username, HighScore){
     // here at the end of a game / game closes the HighScore is checked and if
     // it was beaten it will update the HighScore
+    var QuerySearch = `SELECT * FROM Logins WHERE Username= '`
+    +Username+`' AND HighScore < '` + HighScore + `'`;
+    Con.query(QuerySearch, function (err, result) {
+      if (err) throw err;
+      // if successfully found a smaller HighScore
+      if (result.length > 0){
+        // then there was a response so we can update the highscore
+        UpdateHighScore(Username, HighScore, function(){
+          if (!err) {
+             callback(null,rows);
+          }else {
+             callback(true,err);
+          };
+        });
+      };
+    });
   }
 };
 
@@ -89,4 +105,13 @@ function LogInTo(Username, Password, callback) {
       return callback(null,false);;
     };
   });
-}
+};
+
+function UpdateHighScore(Username, HighScore, callback){
+  var QueryUpdate = `UPDATE Logins SET HighScore = '`
+  +HighScore+`' WHERE Username = '`+Username+`'`;
+  Con.query(QueryUpdate, function(err, result){
+    callback(null,true);;
+  });
+
+};
